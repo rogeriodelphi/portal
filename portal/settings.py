8ee20b5bcfad3eb41956609b873d9f23011353d2
rogeriodelphi <rogeriodelphi@gmail.com>
2023-07-17 17:35:10 -0300

    Configurando CORS_HEADERS

--- portal/settings.py
+++ portal/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from corsheaders.defaults import default_headers
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,6 +28,13 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CORS_ALLOW_HEADERS = list(default_headers) + [
+    'x-Register',
+]
+
+# Cors Config
+CORS_ALLOW_CREDENTIALS: True
+CORS_ALLOW_ALL_ORIGINS: False
 
 # Application definition
 
@@ -38,10 +46,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'base',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
