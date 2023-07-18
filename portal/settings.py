5241d434077798130f39fd514636b531e06f7f0a
rogeriodelphi <rogeriodelphi@gmail.com>
2023-07-18 00:44:00 -0300

    Configurando o Backend e-mail console

--- portal/settings.py
+++ portal/settings.py
@@ -125,6 +125,7 @@ USE_L10N = True
 
 USE_TZ = True
 
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
