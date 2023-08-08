f922122b448d58026230e0df5af95057e7ed0d53
rogeriodelphi <rogeriodelphi@gmail.com>
2023-08-08 00:31:52 -0300

    Autenticação e permissões - Implementando o Password Chance Done

--- contas/urls.py
+++ contas/urls.py
@@ -7,6 +7,9 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('novo-usuario/', UserCreateView.as_view(), name='user_new'),
 
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(
+        template_name='reset/password_change_done.html'), name='password_change_done'
+    ),
     path('password-reset/', auth_views.PasswordResetView.as_view(
         template_name='reset/password_reset_form.html'), name='password_reset'
     ),
